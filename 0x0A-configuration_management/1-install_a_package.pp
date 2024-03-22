555716c18fb1397f3fb168a023c3b6dbbcc8ffcf
Emm_Forster <122850466+Emmie05@users.noreply.github.com>
2024-03-22 20:01:41 +0000

    Config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-# Installs flask, version 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask -v 2.1.0
 
+exec { 'flask':
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+}
