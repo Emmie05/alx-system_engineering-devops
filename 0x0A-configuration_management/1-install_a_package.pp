4bce3bf3e0a777002c310599fb213c89815678a6
Emm_Forster <122850466+Emmie05@users.noreply.github.com>
2024-03-22 19:31:48 +0000

    c

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,5 @@
-#!/usr/bin/pup
-# install an especific version of flask (2.1.0)
-# package {'flask':
-#  ensure   => '2.1.0',
-#  provider => 'pip3'
-#}
-
-class { 'python':
-  version => 'system',
-}
-
-package { ['python3-pip', 'python3']:
-  ensure => installed,
-}
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
