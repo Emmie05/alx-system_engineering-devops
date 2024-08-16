1dd73d9c707716729edcba64017ab5401f716c90
Emm_Forster <122850466+Emmie05@users.noreply.github.com>
2024-08-16 02:34:54 +0000

    11

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-""" This module contains the function top_ten. """
-
-
+"""
+    this module contains the function top_ten
+"""
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
     """
-        Prints the titles of the first 10 hot posts listed for a given subreddit.
-
-        Args:
-            subreddit (str): The subreddit to query.
+        returns the top ten posts for a given subreddit
     """
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post.get('data', {}).get('title', ''))
-            else:
-                print(None)
-        except ValueError:
-            print(None)
-    else:
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
